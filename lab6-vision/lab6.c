9268574c36e83332ac66d920e6e45a78509f3c33
Darin Beaudreau <darinbeaudreau@gmail.com>
2017-03-05 15:59:12 -0500

    Robot now rotates left continuously until it sees the red ball. Then it stops in place.

--- lab6-vision/lab6.c
+++ lab6-vision/lab6.c
@@ -3,17 +3,53 @@
 #define LMOTOR 3
 #define RMOTOR 0
 
+#define SERVO 0
+
+#define SERVO_LEFT 800
+#define SERVO_RIGHT 1900
+
+void seekObject();
 void followObject();
+void kick();
+void rotateLeft();
 
 int main() {
-	followObject();
+	enable_servos();
+	set_each_analog_state(1,0,0,0,0,0,0,0);
+	set_servo_position(SERVO, SERVO_LEFT);
+	msleep(500);
+	
+	seekObject();
 	ao();
 }
 
-void followObject() {
-	int leftValue, rightValue, numberOfBlobs;
+void seekObject() {
+	// Continuously turn in a circle until it sees a ball.
+	// Press the black button to terminate search.
 	while(!black_button()) {
 		track_update();
-		track_count(3);
+		if(track_size(0, 0) > (40 * 40)) {
+			followObject();
+		} else {
+			rotateLeft();
+		}
 	}
+}
+
+void followObject() {
+	while(1) {
+		
+	}
+}
+
+void kick() {
+	set_servo_position(SERVO, SERVO_RIGHT);
+	msleep(500);
+	set_servo_position(SERVO, SERVO_LEFT);
+	msleep(500);
+}
+
+void rotateLeft() {
+	mav(LMOTOR, -300);
+	mav(RMOTOR, 300);
 }
