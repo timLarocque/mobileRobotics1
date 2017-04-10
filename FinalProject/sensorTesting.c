61577dd80594c0b2e616cc629e4f9507ad714fac
Victoria Albanese <Victoria_Albanese@student.uml.edu>
2017-04-10 11:17:23 -0400

    Added if statements

--- FinalProject/sensorTesting.c
+++ FinalProject/sensorTesting.c
@@ -14,4 +14,25 @@ int main()
 	enable_servos();
 	set_each_analog_state(1,0,0,0,0,0,0,0); // enable floating analog port for port 0
 	
+	// keep moving forward
+	mrp(0, 200, -50);
+	mrp(3, 200, 50);
+		
+	// if black is detected on right side
+	if (analog10(RMOTOR) > 500) {
+		
+		// turn left a little
+		mrp(0, 300, 50); 
+		mrp(3, 250, 50);
+	
+	}
+		
+	// if black is detected on left side
+	if (analog10(LMOTOR) > 500) {
+	
+		// turn right a little
+		mrp(0, 300, -50); 
+		mrp(3, 250, -50);
+		
+	}	
 }
