310aa0aab229bfce288f0a7b76d4de81ecf751fe
timLarocque <timothy_larocque@student.uml.edu>
2017-04-10 11:37:16 -0400

    turned mrp to mav and added else if statements

--- FinalProject/sensorTesting.c
+++ FinalProject/sensorTesting.c
@@ -15,25 +15,22 @@ int main()
 	set_each_analog_state(1,0,0,0,0,0,0,0); // enable floating analog port for port 0
 	
 	while(!black_button()) {
-		// keep moving forward
-		mav(LMOTOR, 300);
-		mav(RMOTOR, 300);
-			
 		// if black is detected on right side
 		if (analog10(RHAT) > 500) {
-			
 			// turn left a little
-			mav(RMOTOR, 300);
-			mav(LMOTOR, 100);
-		
-		}
-			
-		// if black is detected on left side
-		if (analog10(LHAT) > 500) {
-		
+			mav(RMOTOR, 500);
+			mav(LMOTOR, -300);		
+		} else if (analog10(LHAT) > 500) { // if black is detected on left side
 			// turn right a little
+			mav(LMOTOR, 500);
+			mav(RMOTOR, -300);
+		} else if {
+		}else {
+			// keep moving forward
 			mav(LMOTOR, 300);
-			mav(RMOTOR, 100);
+			mav(RMOTOR, 300);
 		}
+		printf("Right top hat: %d\n", RHAT);
+		printf("Left top hat:  %d\n", LHAT);
 	}	
 }
