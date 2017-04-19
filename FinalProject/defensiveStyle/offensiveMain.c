7d5cf8d815ec5c062e66cbfa030bc57f6ba5bc71
Victoria Albanese <Victoria_Albanese@student.uml.edu>
2017-04-19 12:51:27 -0400

    Add files via upload

---
+++ FinalProject/defensiveStyle/offensiveMain.c
@@ -0,0 +1,36 @@
+// Created on Wed Apr 12 2017
+
+#include "offensiveStyle.h"
+#include "offensiveStyle.c"
+
+int main() {
+	
+	// initialize shield position
+	Shield target = findShield();
+		
+	// unless you hit the black button...
+	while (!black_button()) {	
+		
+		// approach the shield
+		approachShield();	
+		
+		// if the shield is tagged
+		if (digital(SHIELD) == 1) {
+			acceptDefeat();
+		}
+		/*
+		// if black is detected on left side, veer right
+		if (analog10(LHAT) > 500) { 
+			avoid_border_left();
+		} 
+		
+		// if black is detected on right side, veer left
+		if (analog10(RHAT) > 500) { 
+			avoid_border_right();
+		}
+		*/
+	}
+	
+	return 0;
+	
+}
