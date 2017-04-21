58e0a681fb6d1a6b1890ffc5ffb32fedae52b0e1
Darin Beaudreau <darinbeaudreau@gmail.com>
2017-04-21 11:41:11 -0400

    Changed name of approachShield function to avoidShield and added lance aiming function.

--- FinalProject/defensiveStyle/defensiveMain.c
+++ FinalProject/defensiveStyle/defensiveMain.c
@@ -4,20 +4,19 @@
 #include "defensiveStyle.c"
 
 int main() {
-	
 	// initialize shield position
 	Shield target = findShield();
 		
 	// unless you hit the black button...
 	while (!black_button()) {	
-		
 		// approach the shield
-		approachShield();	
+		avoidShield();	
 		
 		// if the shield is tagged
 		if (digital(SHIELD) == 1) {
 			acceptDefeat();
 		}
+		
 		/*
 		// if black is detected on left side, veer right
 		if (analog10(LHAT) > 500) { 
@@ -27,8 +26,7 @@ int main() {
 		// if black is detected on right side, veer left
 		if (analog10(RHAT) > 500) { 
 			avoid_border_right();
-		}
-		*/
+		}*/
 	}
 	
 	return 0;
