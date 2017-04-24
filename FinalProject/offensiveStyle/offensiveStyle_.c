85cf9570dbd0a0e5f3957e19542456859e6571b0
timLarocque <timothy_larocque@student.uml.edu>
2017-04-24 11:23:48 -0400

    Accidental change of removing whitespace

--- FinalProject/offensiveStyle/offensiveStyle_.c
+++ FinalProject/offensiveStyle/offensiveStyle_.c
@@ -11,7 +11,7 @@ Shield findShield() {
 
 	// update the image
 	track_update();
-	
+
 	// make new shieled, update fields
 	Shield target;
 	target.xCentroid = track_x(GREEN, 0);
@@ -20,24 +20,24 @@ Shield findShield() {
 
 	// return updated shield
 	return target;
-	
+
 }
 
 void approachShield() {
 
 	// find the shield
 	Shield target = findShield();
-	
+
 	// move lance
-	moveLance();		
-	
+	moveLance();
+
 	// if the sheild is offscreen, go forward
-	if (target.size < 50) {	
+	if (target.size < 50) {
 		target = findShield();
 		mav(RMOTOR, 100);
 		mav(LMOTOR, 100);
 	}
-		
+
 	// otherwise, approach the shield
 	else {
 		NormalizedSpeed norm;
@@ -45,32 +45,32 @@ void approachShield() {
 		mav(LMOTOR, norm.left);
 		mav(RMOTOR, norm.right);
 	}
-		
+
 }
 
 NormalizedSpeed normalize(Shield target) {
-	
+
 	NormalizedSpeed temp;
 	temp.left = (int)(((double)target.xCentroid / 160.0) * 500.0);
 	temp.right = (int)(500 - (((double)target.xCentroid / 160.0) * 500.0));
-		
+
 	return temp;
 
 }
 
 void acceptDefeat() {
-	
+
 	// beep a few times in pain
 	beep();
 	beep();
 	beep();
-	
+
 	// curve left/backwards
 	mrp(RMOTOR, 1000, -800);
 	mrp(LMOTOR, 1000, -800);
 	bmd(RMOTOR);
 	bmd(LMOTOR);
-	
+
 	// turn left a lot to face away from opponent
 	mrp(RMOTOR, 1000, 800);
 	mrp(LMOTOR, 1000, -800);
@@ -80,7 +80,7 @@ void acceptDefeat() {
 	while(!black_button()) {
 			ao();
 	}
-	
+
 }
 
 void moveLance() {
@@ -90,33 +90,33 @@ void moveLance() {
 		msleep(100);
 		set_servo_position(LANCE, 80);
 		msleep(100);
-	
+
 }
 
 void avoid_border_left() {
-	
+
 	mrp(RMOTOR, 500, -500);
 	mrp(LMOTOR, 500, -500);
 
 	bmd(LMOTOR);
 	bmd(RMOTOR);
 
-	mrp(LMOTOR, 500, 400); 
+	mrp(LMOTOR, 500, 400);
 	bmd(LMOTOR);
-	
+
 }
 
 void avoid_border_right() {
-	
+
 	mrp(RMOTOR, 500, -500);
 	mrp(LMOTOR, 500, -500);
 
 	bmd(LMOTOR);
 	bmd(RMOTOR);
-			
-	mrp(RMOTOR, 500, 400); 
+
+	mrp(RMOTOR, 500, 400);
 	bmd(RMOTOR);
-	
+
 }
 
 #endif
